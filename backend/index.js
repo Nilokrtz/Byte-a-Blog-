a0db90099e52b79d7e85013ad6bcd1ac5f282007
Nilo <nilogabriel21@gmail.com>
2026-01-01 16:14:18 -0300

    refactor: added the software architecture

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const userRoute = require('./src/routes/user.route');
 const app = express()
 const port = 3000
 
@@ -6,6 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(express.json());
+app.use('/user', userRoute);
+
+app.listen(port);
