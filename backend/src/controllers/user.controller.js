04d85381baf12694c1f66aaa8ca7ef1bea369e2d
Nilo <nilogabriel21@gmail.com>
2026-01-01 19:10:17 -0300

    feat: findAll and findById for User

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,8 @@
+const { default: mongoose } = require("mongoose");
+const { find } = require("../models/User");
 const userService = require("../services/user.service");
 
-const createUser = async (req, res) => {
+const create = async (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
 
   if (!name || !username || !email || !password || !avatar || !background) {
@@ -29,4 +31,36 @@ const createUser = async (req, res) => {
     });
 };
 
-module.exports = { createUser };
+const findAll = async (req, res) => {
+  const users = await userService.findAll();
+
+  if (users.length === 0) {
+    return res.status(400).send({ message: "There are no users" });
+  }
+
+  res.status(200).send({
+    message: "Users found successfully",
+    users,
+  });
+};
+
+const findById = async (req, res) => {
+  const id  = req.params.id;
+
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Invalid user ID" });
+  }
+
+  const user = await userService.findById(id);
+
+  if (!user) {
+    return res.status(400).send({ message: "User not found" });
+  }
+
+  res.status(200).send({
+    message: "User found successfully",
+    user,
+  });
+};
+
+module.exports = { create, findAll, findById };
